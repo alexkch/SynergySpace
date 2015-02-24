6317a8c6b03e9c8e36d3d6bf45a16ab959e6ac22
Kevin Bath <bathalex210@gmail.com>
2015-02-24 12:59:04 -0500

    Basic index.php
    Added index.php to use Heroku

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>SynergySpace</title>
+</head>
+
+<body>
+	<header></header>
+	<nav></nav>
+	<aside></aside>
+	<section></section>
+	<footer>SynergySpace is a coworking space rental and teaming to succeed service. &copy; 2015</footer>
+</body>
+
+</html>
