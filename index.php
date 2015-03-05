df4d9f586755ab94dcc59dbb281593ea6848ecc5
Kevin Bath <bathalex210@gmail.com>
2015-03-05 08:59:52 -0500

    Made global.css and edited all files to import

--- index.php
+++ index.php
@@ -3,12 +3,32 @@
 <head>
 <meta charset="UTF-8">
 <title>SynergySpace</title>
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'> <!-- Google Font Import -->
+<link rel="stylesheet" href="CSS/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/global.css"> <!-- Global CSS Styling -->
 </head>
 
 <body>
-	<header></header>
-	<nav></nav>
-	<aside></aside>
+	<header>
+		<a href="/index.php"><span class="fa fa-connectdevelop fa-2x"></span><span>SynergySpace</span></a>
+		<div id='search-box'>
+			<form action='/search' id='search-form' method='get' target='_top'>
+				<input id='search-text' name='q' placeholder='Search by postal code' type='text' autocomplete="off"/>
+				<button id='search-button' type='submit'>                     
+					<span class="fa fa-search"></span>
+				</button>
+			</form>
+		</div>
+		<div id="account">
+			<ul>
+				<li><a href="/profile.html"><span class="fa fa-cogs"></span>My Account</a></li>
+				<li><a href=""><span class="fa fa-plus"></span>Create an Account</a></li>
+				<li><a href=""><span class="fa fa-sign-out"></span>Logout</a></li>
+				<li><a href=""><span class="fa fa-sign-in"></span>Login</a></li>
+			</ul>
+		</div>
+	</header>
 	<section></section>
 	<footer><a href="https://synergyspace309.herokuapp.com/">SynergySpace</a> is a coworking space rental and teaming to succeed service. &copy; 2015</footer>
 </body>
--- registration.php
+++ registration.php
@@ -7,6 +7,7 @@
 
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'> <!-- Google Font Import -->
 <link rel="stylesheet" href="CSS/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/global.css"> <!-- Global CSS Styling -->
 </head>
 
 <body>
