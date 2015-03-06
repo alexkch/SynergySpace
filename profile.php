d7f32fdad5f47f28c66cd8cad7c5ca5a8379582c
Kevin Bath <bathalex210@gmail.com>
2015-03-06 11:07:39 -0500

    Made profile.html profile.php

--- profile.php
+++ profile.php
@@ -40,7 +40,7 @@ $username = $_SESSION['username'];
 		</div>
 		<div id="account">
 			<ul>
-				<li><a href="/profile.html"><span class="fa fa-cogs"></span>My Account</a></li>
+				<li><a href="/profile.php"><span class="fa fa-cogs"></span>My Account</a></li>
 				<li><a href="/register.php"><span class="fa fa-plus"></span>Create an Account</a></li>
 				<li><a href="/login.php"><span class="fa fa-sign-out"></span>Logout</a></li>
 				<li><a href="/login.php"><span class="fa fa-sign-in"></span>Login</a></li>
