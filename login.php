5e7e6dd2af81d4305750bf8e533f926f5cf53b50
Kevin Bath <bathalex210@gmail.com>
2015-03-06 10:38:25 -0500

    Added more error checking

--- login.php
+++ login.php
@@ -19,10 +19,10 @@ function SignIn() {
 	$userName = $_POST['user']; 
 	$password = md5($_POST['pass']); 
 	if(!empty($_POST['user'])) { 
-		$query = "SELECT * FROM users WHERE username = '$userName' AND pass = '$password';";
-		$result = pg_query($dbconn, $query);
+		$query = "SELECT * FROM users WHERE username='$userName' AND pass='$password';";
+		$result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
 		if(pg_num_rows($result) != 1) {
-			echo "Login Failed!" . pg_num_rows($result);
+			echo "Login Failed! For user: " . $userName . "/" . $password;
 		} else {
 			echo "Login Successful!" . pg_num_rows($result);
 		}
