8758cdcc28bac0fd3f7a901aceeedbed39426eab
Kevin Bath <bathalex210@gmail.com>
2015-03-06 09:14:57 -0500

    Pointed all relevant header nav links to register

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		<div id="account">
 			<ul>
 				<li><a href="/profile.html"><span class="fa fa-cogs"></span>My Account</a></li>
-				<li><a href=""><span class="fa fa-plus"></span>Create an Account</a></li>
+				<li><a href="/register.php"><span class="fa fa-plus"></span>Create an Account</a></li>
 				<li><a href=""><span class="fa fa-sign-out"></span>Logout</a></li>
 				<li><a href=""><span class="fa fa-sign-in"></span>Login</a></li>
 			</ul>
