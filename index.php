14a5b0350106ae1c2ffc65d6f84caf4107221a8e
Kevin Bath <bathalex210@gmail.com>
2015-03-09 14:38:04 -0400

    Fixed styling errors, styled register form

--- index.php
+++ index.php
@@ -33,10 +33,10 @@
 		</div>
 		<div id="account">
 			<ul>
-				<li><a href="/profile.html"><span class="fa fa-cogs"></span>My Account</a></li>
-				<li><a href=""><span class="fa fa-plus"></span>Create an Account</a></li>
-				<li><a href=""><span class="fa fa-sign-out"></span>Logout</a></li>
-				<li><a href=""><span class="fa fa-sign-in"></span>Login</a></li>
+				<li><a href="/profile.php"><span class="fa fa-cogs"></span>My Account</a></li>
+				<li><a href="/register.php"><span class="fa fa-plus"></span>Create an Account</a></li>
+				<li><a href="/login.php"><span class="fa fa-sign-out"></span>Logout</a></li>
+				<li><a href="/login.php"><span class="fa fa-sign-in"></span>Login</a></li>
 			</ul>
 		</div>
 	</header>
--- register.php
+++ register.php
@@ -8,6 +8,7 @@
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'> <!-- Google Font Import -->
 <link rel="stylesheet" href="CSS/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="CSS/global.css"> <!-- Global CSS Styling -->
+<link rel="stylesheet" type="text/css" href="CSS/register.css"> <!-- Register CSS Styling -->
 </head>
 <?php
 
