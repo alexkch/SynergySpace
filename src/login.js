955fa844d243967850f0b42e3fa8561591fce34b
Kevin Bath <bathalex210@gmail.com>
2015-03-09 17:27:30 -0400

    Comparing Strings with indexOf

--- src/login.js
+++ src/login.js
@@ -1,5 +1,5 @@
 	$(function() { //Document Ready Function
-		if(window.location.hash.localeCompare("#user=")==0) {
+		if(window.location.hash.indexOf("#user=")==0) {
 			var username = window.location.hash.split("=")[1];
 			document.getElementById('user').value=username;
 		}
