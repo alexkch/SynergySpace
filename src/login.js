27624aa0f74ba99657ccefc38d7633e0349506f0
Kevin Bath <bathalex210@gmail.com>
2015-03-10 11:38:56 -0400

    Fixed braces in login.js

--- src/login.js
+++ src/login.js
@@ -1,17 +1,17 @@
-	$(function() { //Document Ready Function
-		var hash = window.location.hash;
-		hash = hash.split("&");
-		for (i = 0; i < hash.length; i++) {}
-			if(hash.indexOf("user=")==0) {
-				document.getElementById('user').value=hash.split("=")[1];
-			} else if (hash.indexOf("loggedout")==0) {
-				document.getElementById('notification').value="Logged out Successfully.";
-			} else if (hash.indexOf("error")==0) {
-				switch (hash.split("=")[1]) {
-					case "0":
-						document.getElementById('notification').value="Username or password taken.";
-						break;
-				}
+$(function() { //Document Ready Function
+	var hash = window.location.hash;
+	hash = hash.split("&");
+	for (i = 0; i < hash.length; i++) {
+		if(hash.indexOf("user=")==0) {
+			document.getElementById('user').value=hash.split("=")[1];
+		} else if (hash.indexOf("loggedout")==0) {
+			document.getElementById('notification').value="Logged out Successfully.";
+		} else if (hash.indexOf("error")==0) {
+			switch (hash.split("=")[1]) {
+				case "0":
+					document.getElementById('notification').value="Username or password taken.";
+					break;
 			}
 		}
-	});
+	}
+});
