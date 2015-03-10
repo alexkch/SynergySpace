8e9cfb244f80fed7d35b3cfce55f8f1809cc8abd
Kevin Bath <bathalex210@gmail.com>
2015-03-10 11:41:32 -0400

    Indexing in login.js corrected

--- src/login.js
+++ src/login.js
@@ -2,12 +2,12 @@ $(function() { //Document Ready Function
 	var hash = window.location.hash;
 	hash = hash.split("&");
 	for (i = 0; i < hash.length; i++) {
-		if(hash.indexOf("user=")==0) {
-			document.getElementById('user').value=hash.split("=")[1];
-		} else if (hash.indexOf("loggedout")==0) {
+		if(hash[i].indexOf("user=")==0) {
+			document.getElementById('user').value=hash[i].split("=")[1];
+		} else if (hash[i].indexOf("loggedout")==0) {
 			document.getElementById('notification').value="Logged out Successfully.";
-		} else if (hash.indexOf("error")==0) {
-			switch (hash.split("=")[1]) {
+		} else if (hash[i].indexOf("error")==0) {
+			switch (hash[i].split("=")[1]) {
 				case "0":
 					document.getElementById('notification').value="Username or password taken.";
 					break;
