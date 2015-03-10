a5053c1cb51d0fdbc9e68502babd3da795174ec1
Kevin Bath <bathalex210@gmail.com>
2015-03-10 13:49:05 -0400

    testing

--- changepass.php
+++ changepass.php
@@ -26,19 +26,17 @@ function updateUser() {
 	$newPass = $_POST['pass']; 
 	$newPassConf = $_POST['passconf'];  
 	$oldPass = $_POST['old'];
-	
-	$query = "SET search_path TO synergy; UPDATE users SET password=md5('$newPass') WHERE password=md5('$oldPass');"; 
+	// Test if new passwords match
+	if (strcmp($newPass, $newPassConf)==0) {
+		if (strcmp($newPass, $oldPass)!=0) {
+			$query = "SET search_path TO synergy; UPDATE users SET password=md5('$newPass') WHERE username='$username';"; 
 			$data = pg_query($query) or die('Query failed: ' . pg_last_error()); 
 			if($data) { //Pass Change successful
 				session_destroy(); //Log out
 				//Go to log-in page, with note 2: 'Account password changed successfully.'
-				header("Location: http://synergyspace309.herokuapp.com/login.php#note=2&");
+				header("Location: http://synergyspace309.herokuapp.com/login.php#note=2&newpass=".$newPass);
 				die();
 			}
-	// Test if new passwords match
-	if (strcmp($newPass, $newPassConf)==0) {
-		if (strcmp($newPass, $oldPass)!=0) {
-			//
 		} else { // New pass is same as old
 			header("Location: http://synergyspace309.herokuapp.com/changepass.php#error");
 			die();
