b383ee582c81407d1a9c04fcfd0eb5ed077d14a2
Kevin Bath <bathalex210@gmail.com>
2015-03-10 22:07:26 -0400

    Fixed errors

--- profile.php
+++ profile.php
@@ -60,14 +60,15 @@ $username = $_SESSION['username'];
 			
 			$query = "SET search_path TO synergy; SELECT * FROM users WHERE username='$username'";
 			$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-			$data = pg_fetch_object($result)
-			echo '<img src="http://www.adtechnology.co.uk/images/UGM-default-user.png">';
-			echo '<h2>'.$data->name.'</h2>';
-			echo '<p>'.$data->occupation.'</p>';
-			echo '<p>'.$data->birthdate.'</p>';
-			echo '<p>'.$data->gender.'</p>';
-			echo '<span class="left bottom">'.$data->homeaddress.'</span>';
-			echo '<span class="right bottom">'.$data->phonenumber.'</span>';
+			while ($data = pg_fetch_object($result)) {
+				echo '<img src="http://www.adtechnology.co.uk/images/UGM-default-user.png">';
+				echo '<h2>'.$data->name.'</h2>';
+				echo '<p>'.$data->occupation.'</p>';
+				echo '<p>'.$data->birthdate.'</p>';
+				echo '<p>'.$data->gender.'</p>';
+				echo '<span class="left bottom">'.$data->homeaddress.'</span>';
+				echo '<span class="right bottom">'.$data->phonenumber.'</span>';
+			}
 		?>
 	</div>
 	</section>
