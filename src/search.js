91000e1b3aa5cfbf4f8c617be13079ac82839d9e
Kevin Bath <bathalex210@gmail.com>
2015-03-11 21:26:50 -0400

    Removed extra search

--- src/search.js
+++ src/search.js
@@ -1,7 +1,7 @@
 $(function() { //Document Ready Function
 	var search = window.location.search;
 	search = search.split("&");
-	for (i = 0; i < searchsearch.length; i++) {
+	for (i = 0; i < search.length; i++) {
 		if (search[i].indexOf("q")!=-1) { // Fill username textbox
 			document.getElementById('search-text').value=search[i].split("=")[1].replace("+", " ");
 		}
