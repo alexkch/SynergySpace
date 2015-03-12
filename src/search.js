aede79b138b4c4625e0c55ea0edda543284b3302
Kevin Bath <bathalex210@gmail.com>
2015-03-11 21:25:07 -0400

    Replacing '+' with ' '

--- src/search.js
+++ src/search.js
@@ -1,9 +1,9 @@
 $(function() { //Document Ready Function
 	var search = window.location.search;
 	search = search.split("&");
-	for (i = 0; i < search.length; i++) {
+	for (i = 0; i < searchsearch.length; i++) {
 		if (search[i].indexOf("q")!=-1) { // Fill username textbox
-			document.getElementById('search-text').value=search[i].split("=")[1];
+			document.getElementById('search-text').value=search[i].split("=")[1].replace("+", " ");
 		}
 	}
 });
