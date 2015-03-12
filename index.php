aa618aa68dc9efb372d6736f19ebe76bc639e6e1
Kevin Bath <bathalex210@gmail.com>
2015-03-11 23:51:51 -0400

    Added content to index

--- index.php
+++ index.php
@@ -30,124 +30,14 @@
 			defaultMenu();
 		}
 	?>
-	<div id="relwrapper">
-		<div class="container">
-			<div class="panel-group" id="accordion">
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						<h4 class="panel-title">
-							<a href="#collpaseone" data-toggle="collapse" data-parent="accordion">Recommended Spaces</a>
-						</h4>
-					</div>
-
-
-					<div id="collpaseone" class="panel-collapse collapse in">
-						<div class="panel-body">
-							<div class="cointainer2">
-                                <div class ="row">
-                                    
-                                    <div class="col-md-4">
-
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort1"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item #1
-                                                <div id="toggleSort1" class="collapse in">
-                                                    <br>
-                                                      <a href="#">Item 1 (maybe add pictures) w/ description</a>
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="col-md-4">
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort2"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item #2
-                                                <div id="toggleSort2" class="collapse in">
-                                                    <br>
-                                                    <a href="#">Item 2</a>
-
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>                        
-                                    
-                                    <div class="col-md-4">
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort3"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item 3
-                                                <div id="toggleSort3" class="collapse in">
-                                                    <br>
-                                                            <a href="#">Item 3</a>
-
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>
-                                    <br><br>
-                                                                        <div class="col-md-4">
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort3"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item 3
-                                                <div id="toggleSort3" class="collapse in">
-                                                    <br>
-                                                            <a href="#">Item 3</a>
-
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>
-
-                                                                                                            <div class="col-md-4">
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort3"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item 3
-                                                <div id="toggleSort3" class="collapse in">
-                                                    <br>
-                                                            <a href="#">Item 3</a>
-
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>
-
-                                                                                                            <div class="col-md-4">
-                                        <div class="list-group list-group-flush" data-toggle="collapse" data-target="#toggleSort3"> 
-                                            <div class="list-group-item">
-                                                <span class="glyphicon glyphicon-chevron-right">
-                                                </span>
-                                                Item 3
-                                                <div id="toggleSort3" class="collapse in">
-                                                    <br>
-                                                            <a href="#">Item 3</a>
-
-                                                </div>
-                                            </div> 
-                                        </div>
-                                    </div>
-
-
-                                </div>
-                            </div>
-						</div>
-					</div>
-				</div>
-			</div>	
-<!-- Latest compiled and minified JavaScript -->
-<script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<section>
+		<div id="splash">
+			<img src="/img/header.jpg"/>
+			<h1>SynergySpace</h1>
+			<p>Collaborate with other workers and find the perfect space.</p>
+			<span class="fa fa-arrow-circle-down fa-4x"></span>
+		</div>
+    </section>
 </body>
-<footer>
-	<a href="https://synergyspace309.herokuapp.com/">SynergySpace</a> is a coworking space rental and teaming to succeed service. &copy; 2015
-</footer>
 </html>
+	
