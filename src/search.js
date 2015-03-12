40a7629b029b8eb3290cb8dce85b9269cec7a7b6
Kevin Bath <bathalex210@gmail.com>
2015-03-11 21:23:18 -0400

    Corrected indexing

--- src/search.js
+++ src/search.js
@@ -2,8 +2,8 @@ $(function() { //Document Ready Function
 	var search = window.location.search;
 	search = search.split("&");
 	for (i = 0; i < search.length; i++) {
-		if (search.indexOf("q")!=-1) { // Fill username textbox
-			document.getElementById('search-text').value=search.split("=")[1];
+		if (search[i].indexOf("q")!=-1) { // Fill username textbox
+			document.getElementById('search-text').value=search[i].split("=")[1];
 		}
 	}
 });
