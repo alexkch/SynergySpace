002afde89b65dbee4210c99c9cefe62f7e60f137
alexkch <alexc0219@gmail.com>
2015-04-05 18:53:46 -0400

    new profile link w/ db

--- functions/menu.php
+++ functions/menu.php
@@ -110,6 +110,7 @@ function userMenu() {
   <ul class="list-unstyled">
     <li><a href="/"><span class="fa fa-home"></span>Home</a></li>
     <li><a href="/profile.php"><span class="fa fa-user"></span>Profile</a></li>
+    <li><a href="/profile2.php"><span class="fa fa-user"></span>Profile2</a></li>
     <li><a href="/newbuilding.php"><span class="fa fa-plus"></span>Post New Space</a></li>
 	<li><a href="/updateuser.php"><span class="fa fa-pencil"></span>Update Account Information</a></li>
 	<li><a href="/changepass.php"><span class="fa fa-unlock-alt"></span>Change Account Password</a></li>
@@ -117,46 +118,5 @@ function userMenu() {
   </ul>
 </div>';
 
-
-		/* OLD DEFAULT ONES
-<header>
-      <a href="/index.php"><span class="fa fa-connectdevelop fa-2x"></span><span>SynergySpace</span></a>
-      <div id="search-box">
-        <form action="/search.php" id="search-form" method="get" target="_top">
-          <input id="search-text" name="q" placeholder="Search by address" type="text" autocomplete=off/>
-          <button id="search-button" type="submit">                     
-            <span class="fa fa-search"></span>
-          </button>
-        </form>
-      </div>
-      <div id="account">
-        <ul>
-          <li><a href="/register.php"><span class="fa fa-plus"></span>Create an Account</a></li>
-          <li><a href="/login.php"><span class="fa fa-sign-in"></span>Login</a></li>
-        </ul>
-      </div>
-    </header>'
-
-
-				'<header>
-			<a href="/index.php"><span class="fa fa-connectdevelop fa-2x"></span><span>SynergySpace</span></a>
-			<div id="search-box">
-				<form action="/search.php" id="search-form" method="get" target="_top">
-					<input id="search-text" name="q" placeholder="Search by address" type="text" autocomplete=off/>
-					<button id="search-button" type="submit">                     
-						<span class="fa fa-search"></span>
-					</button>
-				</form>
-			</div>
-			
-			<div id="account">
-				<ul>
-					<li><a href="/profile.php"><span class="fa fa-cogs"></span>My Account</a></li>			
-					<li><a href="/login.php"><span class="fa fa-sign-out"></span>Logout</a></li>
-					<li><a class="nav_trigger" href="#"><span class="fa fa-user"></span>Menu</a></li>
-				</ul>
-			</div>
-		</header>
-*/
 }
 ?>
